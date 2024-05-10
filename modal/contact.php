0f79a640880e800f3f716d225e02ce47ad4a313e
Ramesh Kumar Pandey <37406985+pandeyramesh677@users.noreply.github.com>
2024-05-10 11:37:31 +0530

    Update contact.php

--- modal/contact.php
+++ modal/contact.php
@@ -1,46 +1,32 @@
 <?php
+include('smtp/PHPMailerAutoload.php');
 
-#Receive user input
-$email_address = $_POST['email'];
-$name = $_POST['name'];
-$message = $_POST['message'];
-
-#Filter user input
-function filter_email_header($form_field) {  
-return preg_replace('/[nr|!/<>^$%*&]+/','',$form_field);
-}
-
-$email_address  = filter_email_header($email_address);
-
-#Send email
-$headers = "From: $email_address";
-$sent = mail('sm6214678@gmail.com', 'Feedback Form Submission', $name, $headers, $message);
-
-#Thank user or notify them of a problem
-if ($sent) {
-
-?><html>
-<head>
-<title>Thank You</title>
-</head>
-<body>
-<h1>Thank You</h1>
-<p>Thank you for your feedback.</p>
-</body>
-</html>
-<?php
-
-} else {
-
-?><html>
-<head>
-<title>Something went wrong</title>
-</head>
-<body>
-<h1>Something went wrong</h1>
-<p>We could not send your feedback. Please try again.</p>
-</body>
-</html>
-<?php
+echo smtp_mailer('to_email','Subject','html');
+function smtp_mailer($to,$subject, $msg){
+	$mail = new PHPMailer(); 
+	$mail->IsSMTP(); 
+	$mail->SMTPAuth = true; 
+	$mail->SMTPSecure = 'tls'; 
+	$mail->Host = "smtp.gmail.com";
+	$mail->Port = 587; 
+	$mail->IsHTML(true);
+	$mail->CharSet = 'UTF-8';
+	//$mail->SMTPDebug = 2; 
+	$mail->Username = "sm6214678@gmail.com";
+	$mail->Password = "Gloomykicker @1#3";
+	$mail->SetFrom("email");
+	$mail->Subject = $subject;
+	$mail->Body =$msg;
+	$mail->AddAddress($to);
+	$mail->SMTPOptions=array('ssl'=>array(
+		'verify_peer'=>false,
+		'verify_peer_name'=>false,
+		'allow_self_signed'=>false
+	));
+	if(!$mail->Send()){
+		echo $mail->ErrorInfo;
+	}else{
+		return 'Sent';
+	}
 }
 ?>
