2ea7e907e81be15e72b55576cb5bdba9799fb51c
Ramesh Kumar Pandey <37406985+pandeyramesh677@users.noreply.github.com>
2024-05-10 13:16:57 +0530

    Update sending_message.js

--- sending_message.js
+++ sending_message.js
@@ -1,32 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
     const contactForm = document.getElementById('contactForm');
     const statusMessage = document.getElementById('statusMessage');
-  
-    contactForm.addEventListener('submit', function (event) {
-      event.preventDefault(); // Prevent the default form submission
-  
-      const formData = new FormData(contactForm);
-  
-      // Make an AJAX request to an external server-side script
-      fetch('https://github.com/pandeyramesh677/rameshpandey.github.io/blob/main/process_form.php', {
-        method: 'POST',
-        body: formData
-      })
-      .then(response => {
-        if (response.ok) {
-          return response.text(); // If the response is OK, return the response text
-        } else {
-          throw new Error('Failed to send message'); // If the response is not OK, throw an error
-        }
-      })
-      .then(data => {
-        // Display success message to the user
-        statusMessage.textContent = 'Your message was sent successfully!';
-      })
-      .catch(error => {
-        // Display error message to the user
-        statusMessage.textContent = 'Failed to send message. Please try again later.';
-      });
-    });
-  });
-  
+
+    if (contactForm) {
+        contactForm.addEventListener('submit', function (event) {
+            event.preventDefault(); // Prevent the default form submission
+
+            const formData = new FormData(contactForm);
+
+            // Make an AJAX request to an external server-side script
+            fetch('https://github.com/pandeyramesh677/rameshpandey.github.io/blob/main/process_form.php', {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => {
+                if (response.ok) {
+                    return response.text(); // If the response is OK, return the response text
+                } else {
+                    throw new Error('Failed to send message'); // If the response is not OK, throw an error
+                }
+            })
+            .then(data => {
+                // Display success message to the user
+                statusMessage.textContent = 'Your message was sent successfully!';
+            })
+            .catch(error => {
+                // Display error message to the user
+                statusMessage.textContent = 'Failed to send message. Please try again later.';
+            });
+        });
+    } else {
+        console.error('Contact form element not found');
+    }
+});
