f194588d2c5ab4149c2516f5da2930a6ee88850d
Ramesh Kumar Pandey <37406985+pandeyramesh677@users.noreply.github.com>
2024-05-10 11:19:13 +0530

    Update contact.php

--- modal/contact.php
+++ modal/contact.php
@@ -1,13 +1,46 @@
 <?php
-//get data from form  
+
+#Receive user input
+$email_address = $_POST['email'];
 $name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
-$to = "sm6214678@gmail.com";
-$subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
+$message = $_POST['message'];
+
+#Filter user input
+function filter_email_header($form_field) {  
+return preg_replace('/[nr|!/<>^$%*&]+/','',$form_field);
+}
+
+$email_address  = filter_email_header($email_address);
+
+#Send email
+$headers = "From: $email_address";
+$sent = mail('sm6214678@gmail.com', 'Feedback Form Submission', $name, $headers, $message);
+
+#Thank user or notify them of a problem
+if ($sent) {
+
+?><html>
+<head>
+<title>Thank You</title>
+</head>
+<body>
+<h1>Thank You</h1>
+<p>Thank you for your feedback.</p>
+</body>
+</html>
+<?php
+
+} else {
+
+?><html>
+<head>
+<title>Something went wrong</title>
+</head>
+<body>
+<h1>Something went wrong</h1>
+<p>We could not send your feedback. Please try again.</p>
+</body>
+</html>
+<?php
 }
 ?>
