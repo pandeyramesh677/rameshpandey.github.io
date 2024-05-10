1a6701519a147127405e4cb6f78df39211749111
Ramesh Kumar Pandey <37406985+pandeyramesh677@users.noreply.github.com>
2024-05-10 13:23:38 +0530

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,20 +1,26 @@
 <?php
-// Retrieve form data
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Retrieve form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-// Process the form data (e.g., send an email)
-$to = 'sm6214678@gmail.com';
-$subject = 'New Message from Contact Form';
-$body = "Name: $name\nEmail: $email\nMessage: $message";
-$headers = "From: $email";
+    // Process the form data (e.g., send an email)
+    $to = 'sm6214678@gmail.com';
+    $subject = 'New Message from Contact Form';
+    $body = "Name: $name\nEmail: $email\nMessage: $message";
+    $headers = "From: $email";
 
-if (mail($to, $subject, $body, $headers)) {
-    // Send a success response to the client-side JavaScript
-    echo 'success';
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        // Send a success response to the client-side JavaScript
+        echo 'success';
+    } else {
+        // Send an error response to the client-side JavaScript
+        echo 'error';
+    }
 } else {
-    // Send an error response to the client-side JavaScript
+    // Return error if accessed via GET request
     echo 'error';
 }
 ?>
